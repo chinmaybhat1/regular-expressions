67a963199c9b50de8648a04512bd99dbfeaa5a5e
Bhat <Chinmay.Bhat@philips.com>
2020-06-02 23:52:05 +0530

    Search conditionally

--- src/com/learning/RegularExpressionDemo.java
+++ src/com/learning/RegularExpressionDemo.java
@@ -3,31 +3,71 @@ package com.learning;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/*
+ * Common regular patterns and their meaning
+ * [abc] : Either a or b or c
+ * [^abc] : Except a or b or c
+ * [a-z] : Any lower case alphabets
+ * [A-Z] : Any upper case alphabets
+ * [0-9] : Any number
+ * [a-zA-Z] : Any alphabet
+ * [a-zA-Z0-9] : Any alpha numeric characters
+ * [^a-zA-Z0-9] : Only special characters
+ */
 public class RegularExpressionDemo {
 
-	public static void main(String[] args) {
-		// Character sequence in which a pattern needs to be searched
-		final String targetString = "ababbaba";
-		// Pattern that needs to be searched
-		final String PATTERN = "ab";
-		// Number of occurrences of a pattern in target string 
-		int matchCount = 0;
+	private static final String TARGET_STRING = "a7b@Z#9";
 
-		// Converts a pattern into a Java object representation
-		Pattern pattern = Pattern.compile(PATTERN);
-		// Converts a target string into a Java object representation
+	private static final String SPECIFIC_ALPHABETS = "[abc]";
+	private static final String EXCEPT_SPECIFIC_ALPHABETS = "[^abc]";
+	private static final String LOWERCASE_ALPHABETS = "[a-z]";
+	private static final String UPPERCASE_ALPHABETS = "[A-Z]";
+	private static final String ALPHABETS = "[a-zA-Z]";
+	private static final String NUMBERS = "[0-9]";
+	private static final String ALPHA_NUMERIC = "[a-zA-Z0-9]";
+	private static final String SPECIAL_CHARACTERS = "[^a-zA-Z0-9]";
+
+	public static void main(String[] args) {
+		
+		printTestCase("Check for either a or b or c in given string " + TARGET_STRING);
+		checkPatternInTargetString(SPECIFIC_ALPHABETS, TARGET_STRING);
+
+		printTestCase("Check for characters other than a or b or c in given string " + TARGET_STRING);
+		checkPatternInTargetString(EXCEPT_SPECIFIC_ALPHABETS, TARGET_STRING);
+
+		printTestCase("Check for lower case alphabets in given string " + TARGET_STRING);
+		checkPatternInTargetString(LOWERCASE_ALPHABETS, TARGET_STRING);
+
+		printTestCase("Check for upper case alphabets in given string " + TARGET_STRING);
+		checkPatternInTargetString(UPPERCASE_ALPHABETS, TARGET_STRING);
+
+		printTestCase("Check for alphabets in given string " + TARGET_STRING);
+		checkPatternInTargetString(ALPHABETS, TARGET_STRING);
+
+		printTestCase("Check for numbers in given string " + TARGET_STRING);
+		checkPatternInTargetString(NUMBERS, TARGET_STRING);
+
+		printTestCase("Check for alpha numeric characters in given string " + TARGET_STRING);
+		checkPatternInTargetString(ALPHA_NUMERIC, TARGET_STRING);
+
+		printTestCase("Check for special characters in given string " + TARGET_STRING);
+		checkPatternInTargetString(SPECIAL_CHARACTERS, TARGET_STRING);
+	}
+
+	private static void printTestCase(String message) {
+		System.out.println("***** " + message + " *****");
+	}
+
+	private static void checkPatternInTargetString(String regx, String targetString) {
+
+		Pattern pattern = Pattern.compile(regx);
 		Matcher matcher = pattern.matcher(targetString);
 
-		// Checks whether a pattern is present in a target string
 		while (matcher.find()) {
-			matchCount++;
-			System.out.println("Match found at start index " + matcher.start() // Returns the start index of a target string matching given pattern
-				+ ", end index " + matcher.end() // Returns the end index + 1 of a target string matching given pattern
-					+ " for pattern " + matcher.group() // Returns the matched sequence captured by the previous match as the string.
-			);
+			System.out.println(matcher.group() + " found at " + matcher.start());
 		}
 
-		System.out.println(PATTERN + " found " + matchCount + " times in " + targetString);
+		System.out.println();
 	}
 
 }
