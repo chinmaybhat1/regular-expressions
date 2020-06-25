9a5dc2dff85afb2259d6af239674787439127bb4
Bhat <Chinmay.Bhat@philips.com>
2020-06-25 06:49:27 +0530

    Pattern class split()

--- src/com/learning/RegularExpressionDemo.java
+++ src/com/learning/RegularExpressionDemo.java
@@ -1,38 +1,19 @@
 package com.learning;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /*
- * Quantifiers: Used to specify number of occurrence of a match
- * 
- * a  - Exactly one a
- * a+ - At least one a
- * a* - At least one or multiple occurrence of a
- * a? - Exactly one or multiple occurrence of a 
+ * Regular Expression split()
  */
 public class RegularExpressionDemo {
 
-	private static final String TARGET_STRING = "abaabbaaabbb";
-
-	private static final String EXACT_MATCHER = "a";
-	private static final String MATCHER_PLUS = "a+";
-	private static final String MATCHER_ASTRISK = "a*";
-	private static final String MATCHER_QUESTION_MARK = "a?";
+	private static final String TARGET_STRING = "www.chinmay.bhat.com";
+	private static final String MATCHER_DOT = "[.]";
 
 	public static void main(String[] args) {
 
-		printTestCase("Exactly one match in given string " + TARGET_STRING);
-		checkPatternInTargetString(EXACT_MATCHER, TARGET_STRING);
-
-		printTestCase("At least one match in given string " + TARGET_STRING);
-		checkPatternInTargetString(MATCHER_PLUS, TARGET_STRING);
-
-		printTestCase("At least one or multiple occurrence in given string " + TARGET_STRING);
-		checkPatternInTargetString(MATCHER_ASTRISK, TARGET_STRING);
-
-		printTestCase("Exactly one or multiple occurrence in given string " + TARGET_STRING);
-		checkPatternInTargetString(MATCHER_QUESTION_MARK, TARGET_STRING);
+		printTestCase("Given string " + TARGET_STRING + " split using space");
+		checkPatternInTargetString(MATCHER_DOT, TARGET_STRING);
 
 	}
 
@@ -43,10 +24,10 @@ public class RegularExpressionDemo {
 	private static void checkPatternInTargetString(String regx, String targetString) {
 
 		Pattern pattern = Pattern.compile(regx);
-		Matcher matcher = pattern.matcher(targetString);
+		String[] tokens = pattern.split(targetString);
 
-		while (matcher.find()) {
-			System.out.println(matcher.group() + " found at " + matcher.start());
+		for (String token : tokens) {
+			System.out.println(token);
 		}
 
 		System.out.println();
